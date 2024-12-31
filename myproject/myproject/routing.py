d6ee97679352c6ed6ab4c7d9987519c4a50137ae
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-12-31 08:20:35 +0800

    chat websocket-work in progress

--- myproject/myproject/routing.py
+++ myproject/myproject/routing.py
@@ -1,6 +1,10 @@
 from django.urls import re_path
 from cart.consumers import OrderConsumer
+from chat.consumers import ChatConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/orders/(?P<order_id>\d+)/$', OrderConsumer.as_asgi()),
+
+    re_path(r'ws/chat/(?P<room_id>\d+)/$', ChatConsumer.as_asgi()),
+
 ]
